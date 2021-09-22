2112a9acaeedaa29b0e24bf9b75abd77978cf56f
Shanoor24 <shanoorfb2403@gmail.com>
2021-09-22 10:37:51 +0530

    Ui updated

--- assignment/src/Components/FeildMapperConfiguration/FieldHeader.jsx
+++ assignment/src/Components/FeildMapperConfiguration/FieldHeader.jsx
@@ -4,7 +4,7 @@ import styles from "./FieldHeader.module.css"
 function FieldHeader() {
     return (
         <div id={styles.Main_cont}>
-            <div>Salesforcefields</div>
+            <div>Salesforce fields</div>
             <div>CallHub custom fields</div>
         </div>
     )
